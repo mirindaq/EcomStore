6e716828fde1c89cf30eeec800806b53ed9a690d
Viet Hoang <gvhoang30112004@gmail.com>
2025-07-23 19:45:51 +0700

    feat(entity): add base entities

---
+++ Back-End/src/main/java/vn/com/ecomstore/EcomstoreApplication.java
@@ -0,0 +1,13 @@
+package vn.com.ecomstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomstoreApplication.class, args);
+	}
+
+}
---
+++ Back-End/src/test/java/vn/com/ecomstore/EcomstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.ecomstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
